a97b35a22f4de11b6a06ea96dcbd1df4b5c17858
Ryszard Brzegowy <rbrzegowy@wsei.edu.pl>
2022-10-20 15:09:02 +0200

    lab3 commit z grupy z 13:30

--- Lab 3 - drumkit/drumkit.js
+++ Lab 3 - drumkit/drumkit.js
@@ -1,18 +1,33 @@
-// document.body
+// notatnik 
 document.addEventListener('keypress', onKeyPress)
 
-function onKeyPress(event) {
-    const key = event.key
-    // ktory dzwiek w zaleznosci od klawisza
-    const sound = 'clap'
+const KeyToSound = {
+    'a': 's1',
+    's': 's2',
+}
+function onKeyPress(ev) {
+    // const key = event.key
+    // logika mapowania key -> sound
+    const sound = KeyToSound[ev.key]
+    // switch (ev.key) {
+    //     case 'a':
+    //         sound = SOUND.clap
+    //         // clap
+    //         break;
+    //     case 's':
+    //         sound = SOUND.hihat
+    //         // hihat
+    //         break;
+    // }
     playSound(sound)
 }
 
-
 function playSound(sound) {
-    const audioTag = document.querySelector('#' + sound)
-    audioTag.currentTime = 0 
+    if (!sound) {
+        return
+    }
+    const audioTag = document.querySelector(`#${clap}`)
+    audioTag.currentTime = 0
     audioTag.play()
 }
-
 // Date.now()
